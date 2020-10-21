d8e5b763b568b46446cbcafe6f819c919c4f87f9
heba189 <abdouheba552@gmail.com>
2020-10-21 12:59:46 +0200

    vv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { createComponent } from '@angular/compiler/src/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { TestFilterComponent } from './test-filter/test-filter.component';
 
 const appRoutes :Routes=[
   {path:'list' ,component:ListEmployeesComponent},
@@ -22,13 +24,15 @@ const appRoutes :Routes=[
   declarations: [
     AppComponent,
     ListEmployeesComponent,
-    CreateEmployeeComponent
+    CreateEmployeeComponent,
+    TestFilterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(appRoutes),
     FormsModule,
+    Ng2SearchPipeModule,
     BsDatepickerModule.forRoot(),
     BrowserAnimationsModule
   ],
---
+++ src/app/test-filter/test-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-filter',
+  templateUrl: './test-filter.component.html',
+  styleUrls: ['./test-filter.component.css']
+})
+export class TestFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
